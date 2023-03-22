d83a3d5891252467efc65f33788becd296818d47
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-03-22 15:57:20 -0700

    prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * Main - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return:0
+ *
+**/
+
+void print_alphabet(void) 
+{
+	char i;
+	
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
