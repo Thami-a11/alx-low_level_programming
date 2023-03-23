01728c19e683a1bb199ba4c100bf05cadec0d90b
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-03-23 13:35:37 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if it is a number
+ *
+ * @c: Parameter input
+ *
+ * Return: 1 if its a digit, else 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,5 @@
 #define _MAIN_
 
 int _isupper(int c);
-
+int _isdigit(int c);
 #endif
