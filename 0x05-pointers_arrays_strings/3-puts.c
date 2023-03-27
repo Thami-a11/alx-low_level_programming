70fbcf5ccb45c92eb9ef87f514a3ea370d4ddc4e
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-03-27 14:38:18 -0700

    Print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
+
 /**
- * _puts - write a string
- *
- *@str: string parameter
- *
-*/ 
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
 
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (; *str != '\0'; str++)
 	{
-		_putchar(str[i]);
+		_putchar(*str);
 	}
 
 	_putchar('\n');
