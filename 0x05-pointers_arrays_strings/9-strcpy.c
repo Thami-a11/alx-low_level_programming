e33e8189b252aa764548968443e57b7525b4ad82
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-03-27 15:54:19 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy the string 
+ * @dest: copy of the string
+ * @src: original string
+ * Return: dest copy
+**/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
