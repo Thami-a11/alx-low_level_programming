ba62de5c746b8b6b37b017eadc03c2f14262de06
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-03-30 17:08:30 -0700

    To upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - To Upper
+ * @str: string
+ * Return: str
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)str[i] - 32;
+	}
+	return (str);
+}
