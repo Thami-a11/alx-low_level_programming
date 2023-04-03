65fb6401ee7569f6e5ac8480affa8eca11d25d98
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-03 06:46:36 -0700

    set memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - assign - the first byte to n
+ * @s: parameter pointer
+ * @b: parameter
+ * @n: int parameter
+ * Return: pointer char
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
