e9b16f387414267038001928dc95b7331a018b2c
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-03 12:30:01 -0700

    Search for char in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,15 +13,16 @@ char *_strchr(char *s, char c)
 	int i;
 
 
-	for (i = 0; i < _strlen(s); i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (&s[i]);
+			return (s);
 		}
 		else
 		{
-			return (NULL);
+			s++;
 		}
 	}
+	return (NULL);
 }
