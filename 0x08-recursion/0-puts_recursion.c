194b7b3edf64c3f0632ab764909905112ea905dc
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-05 12:43:35 -0700

    print string with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ * @s: char parameter
+ *
+*/
+
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
+
