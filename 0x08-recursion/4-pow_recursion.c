d44cc85439e75282ee9598b4d998b868dd9eb255
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-05 14:49:46 -0700

    power of

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - get power of
+ * @x: base parameter
+ * @y: Power parameter
+ * Return: power of
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+		return (-1);
+	else if (y == 0)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
