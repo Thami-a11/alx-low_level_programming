23de3f2f0bc74ca19e6e49d5f0526c761d80218f
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-10 13:56:18 -0700

    Silence is argument carried out by other means" Silence is argument carried out by other means
    q
    close
    Silence is argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - get number of arguments passed
+ * @argc: Counter
+ * @argv: array of argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	prinf("%d\n", argc);
+	return (0);
+}
