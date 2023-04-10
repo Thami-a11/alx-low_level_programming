ed0cc4432f70fb137bdec2c3b449a565c49b074f
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-10 15:56:56 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - write all arguments
+ * @argc: argument count
+ * @argv: array of argument
+ * Return:0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int added, i;
+
+	added = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		if (isdigit(argv[i]))
+		{
+			added += argv[i];
+			printf("%d\n", added);
+			return (0);
+		}
+		else
+		{
+			printf("Error\n");
+			return (0);
+		}
+	}
+}
