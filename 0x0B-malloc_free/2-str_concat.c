3c29b343a2c66ff9d290f446d040242ea5b5be3f
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-11 14:29:56 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *str_concat - concat two strings and allocate memory
+ *
+ *@s1: 1st string
+ *@s2: 2st string
+ *Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int ls1, ls2, i, j, Flen;
+
+	char *c;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return ("");
+	}
+
+	ls1 = strlen(s1);
+	ls2 = strlen(s2);
+	Flen = ls1 + ls2 + 1;
+
+	c = malloc(sizeof(char) * Flen);
+
+	if (c == NULL)
+		return (NULL);
+
+	for (i = 0; i <= ls1; i++)
+	{
+		c[i] = s1[i];
+	}
+	for (j = 0; j <= s2; j++)
+	{
+		c[ls1 + j] = s2[j];
+	}
+	return (c);
+}
