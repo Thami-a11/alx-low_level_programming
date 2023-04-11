fcc30946bddde6d029044c6626932ab5701fe8bc
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-11 13:32:50 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - copy string and allocate to memory
+ * @str: input string
+ *
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	int l;
+
+	char *c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	l = _strlen(*str) + 1;
+	c = malloc(l * sizeof(char));
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	return (c);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *create_array(unsigned int size, char c);
+int _strlen(char *s);
+char *_strdup(char *str);
 #endif
