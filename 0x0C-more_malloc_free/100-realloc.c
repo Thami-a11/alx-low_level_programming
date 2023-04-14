42243b7abce7d8e1f7788722d7b6ae082a6ed615
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-13 18:20:59 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		str = malloc(new_size);
 		free(ptr);
-		return (s);
+		return (str);
 	}
 	else if (new_size == old_size)
 	{
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		str = malloc(new_size);
 		free(ptr);
-		return (s);
+		return (str);
 	}
 	else if (new_size == 0 && ptr != NULL)
 	{
