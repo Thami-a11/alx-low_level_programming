078ccbfe8eb991f0830f5c4107a53005023121fe
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-17 13:04:10 -0700

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file it was compiled from.
- * @argc: int
- * @argv: array
+ * main - prints the file it was compiled from.
+ *
  * Return: Always 0 (Success).
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	printf("%s\n", argv[0]);
-	(void)argc;
+	printf("%s\n", __FILE__);
 	return (0);
 }
