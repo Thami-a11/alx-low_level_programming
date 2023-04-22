120f1a3f9562218bd6f5edc9caf90edd93f990ab
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-22 11:09:11 -0700

    calculations with pointer

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - Adds two numbers
+ * @a: int
+ * @b: int
+ * Return: a + b
+**/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: int
+ * @b: int
+ * Return: a - b
+**/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ * @a: int
+ * @b: int
+ * Return: a * b
+**/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers
+ * @a: int
+ * @b: int
+ * Return: a / b
+**/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modules two numbers
+ * @a: int
+ * @b: int
+ * Return: a % b
+**/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
