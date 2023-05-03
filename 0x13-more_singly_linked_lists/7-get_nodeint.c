0a4e2eae2616a8e2598b2a6dddbd6dfd46ddbd72
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-05-03 15:57:52 -0700

    Get nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,20 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *current;
 
+	unsigned int i;
+
+	i = 0;
+
+	current = head;
+
 	while (current != NULL)
 	{
-		if (current->n == index)
+		if (i == index)
 			return (current);
+
+		i++;
 
 		current = current->next;
 	}
-	return (current);
+	return (NULL);
 }
